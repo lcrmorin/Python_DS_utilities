9bb06c9ac3020051da0dc2a69a22770976b0c849
lcrmorin <lucas.cr.morin@gmail.com>
2023-05-05 15:43:32 +0200

    Update numpy_groupy_corr.py
    adding spearman correlation

--- numpy_groupy_corr.py
+++ numpy_groupy_corr.py
@@ -2,17 +2,24 @@ from joblib import Parallel, delayed
 import numpy as np
 from scipy import stats
 
-eras = ['Moon']
+def process_pearson(x):
+    return np.corrcoef(x.T)
 
-def process(x):
-    return stats.spearmanr(x).correlation
+def process_spearman_np(x):
+    return np.corrcoef(stats.rankdata(x,axis=0).T)
 
-def groupby_corr_numpy(df, eras, cols = all_features+targets):
+def groupby_corr_numpy(df, features, targets, eras, func):
+    cols = features + targets
     ids, index = np.unique(df[eras], return_index=True)
     splits = np.split(df[cols].values, index[1:])
-    ret = Parallel()(delayed(process)(x) for time_id, x in zip(ids.tolist(), splits))
-    ret = pd.concat([pd.DataFrame(r, index=cols,columns=cols) for r in ret], keys=ids)
+    ret = Parallel()(delayed(func)(x) for time_id, x in zip(ids.tolist(), splits))
+    ret = pd.concat([pd.DataFrame(r, index=cols, columns=cols) for r in ret], keys=ids)
     ret.index = ret.index.set_names([eras[0], 'features'])
     return ret
 
-corrs_by_moons = groupby_corr_numpy(train, eras)
+all_data = pd.concat([train_data, train_targets[targets]], axis=1)
+
+#%%time
+#corrs_by_moons = groupby_corr_numpy(all_data, features, targets, eras, process_pearson)
+corrs_by_moons = groupby_corr_numpy(all_data, features, targets, eras, process_spearman_np)
+corrs_by_moons.head()
