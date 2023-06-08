53921338f8db4c09aea8bbe3c03c190ac3221cb0
lcrmorin <lucas.cr.morin@gmail.com>
2023-06-08 12:34:17 +0200

    Update data_viz.py
    better precision formatting

--- data_viz.py
+++ data_viz.py
@@ -2,11 +2,12 @@ import pandas as pd, numpy as np
 from collections import defaultdict
 import seaborn as sns
 
+# base dataframe formatting
 df = pd.DataFrame({'feature':[-1,0,1,12],'target':[0.0012, 0.021, 0, np.nan]})
 col = df.columns
 
-dict_format =  defaultdict(lambda: {})
-dict_format.update({'target': '{0:.2%}'})
+dict_format =  defaultdict(lambda: "{:.0}")
+dict_format.update({'target': '{:.2%}'})
 
 dict_color =  defaultdict(lambda: 'Blues')
 dict_color.update({'feature': 'Spectral'})
@@ -18,9 +19,10 @@ df_style = df.style
 
 for c in col:
     df_style = df_style.background_gradient(subset=c, cmap=dict_color[c], 
-                                            vmin=dict_v[c][0], vmax=dict_v[c][1]).format(dict_format[c])
+                                            vmin=dict_v[c][0], vmax=dict_v[c][1])
+  
+df_style.format(dict_format).applymap(lambda x: 'background: lightgrey; color: transparent' if pd.isnull(x) else '')
 
-df_style.applymap(lambda x: 'background: lightgrey' if pd.isnull(x) else '').applymap(lambda x: 'color: transparent' if pd.isnull(x) else '')
 # base dependance plot
 
 sns.regplot(x=df[feature], 
