94edb6f8755f490ba98c923c06294d9198681adc
Lucas Morin <lucas.cr.morin@gmail.com>
2023-08-25 10:16:56 +0200

    Update pandas_filter_on_read.py

--- pandas_filter_on_read.py
+++ pandas_filter_on_read.py
@@ -2,3 +2,8 @@ import pandas as pd
 
 iter_csv = pd.read_csv('file.csv', iterator=True, chunksize=1000)
 df = pd.concat([chunk[chunk['field'] > constant] for chunk in iter_csv])
+
+with open(<file>) as f:
+    text = "\n".join([line for line in f if line.split(',')[2] == "Hello"])
+
+df = pd.read_csv(StringIO(text))
